aa942ec235e57a9d384d78af12ad39c51b818b89
Venkatakula123 <akulavenkateswarreddy@gmail.com>
2025-12-09 16:28:37 +0530

    day1

---
+++ Variables/Operators.py
@@ -0,0 +1,17 @@
+a = 46    # Initializing the value of a      
+b = 4     # Initializing the value of b      
+    
+print("For a =", a, "and b =", b,"\nCheck the following:")    
+    
+# printing different results    
+print('1. Two numbers are equal or not:', a == b)      
+print('2. Two numbers are not equal or not:', a != b)      
+print('3. a is less than or equal to b:', a <= b)      
+print('4. a is greater than or equal to b:', a >= b)      
+print('5. a is greater than b:', a > b)      
+print('6. a is less than b:', a < b)   
+
+v1 = 'Akula'
+v2 = 'Aakula'
+
+print(v1 == v2)
---
+++ Variables/Variables.py
@@ -0,0 +1,28 @@
+num = 20
+print(num)
+print(type(num))
+print(id(num))
+num = 'Venkat'
+print(num)
+print(type(num))
+print(id(num))
+
+a = 10
+b = a
+print("a is",a)
+print("a id is at 10 ",id(a))
+print("B is",b)
+print("b id is at 10",id(b))
+
+k =10
+print("k id is at 10 ",id(k))
+
+i  = 'Venkat'
+print("i id is at Venkat",id(i))
+
+a = 9
+print("a at again ",a)
+print("a id is at 9",id(a))
+k = a
+print("k is after a ", k)
+print("K id at 9 ",id(k))
