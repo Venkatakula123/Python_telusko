826481fe7d003a052bdde0b7beeaeb3b0c8351e5
Venkatakula123 <akulavenkateswarreddy@gmail.com>
2025-12-09 17:33:23 +0530

    changes

--- Variables/Operators.py
+++ Variables/Operators.py
@@ -13,5 +13,24 @@ print('6. a is less than b:', a < b)
 
 v1 = 'Akula'
 v2 = 'Aakula'
-
 print(v1 == v2)
+
+a = 2
+a += 2
+print(a)
+
+a = a ** 2
+print(a)
+
+
+b = 2
+c = b ** 2
+print(c)
+
+print(bin(25)) #0-1
+print(0b11001) 
+print(oct(25)) # 0-7
+print(hex(10)) # 0-9 and a-f
+
+print(bin(25),bin(30))
+print(0b11000)
