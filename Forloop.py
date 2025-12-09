f579fe1a22cab31954d86bc65a27c2035415abce
Venkatakula123 <akulavenkateswarreddy@gmail.com>
2025-12-09 18:47:05 +0530

    Few changes

---
+++ Forloop.py
@@ -0,0 +1,7 @@
+num = int(input("Enter your number:  "))
+
+for i in range(0,num):
+    print(i)
+
+for i in range(1,num):
+    print(i)
---
+++ Temparature.py
@@ -0,0 +1,14 @@
+temp = int(input("Enter Temprature in your City"))
+
+if temp >= 40:
+    print("It's hot day")
+elif temp >= 30:
+    print("It is Mid day")
+elif temp > 25:
+    print("It's cool day")
+elif temp > 20:
+    print("It's winter")
+elif temp > 10:
+    print("Not allowed outside")
+elif temp < 0:
+    print("You will be dead don't come outside any cost")
