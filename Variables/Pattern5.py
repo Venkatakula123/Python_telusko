e49bf349c60b79bc7881b9aca536724e7ac4465d
Venkatakula123 <akulavenkateswarreddy@gmail.com>
2025-12-10 17:30:03 +0530

    avr

---
+++ Variables/Pattern5.py
@@ -0,0 +1,9 @@
+n = 5
+for i in range(1, n + 1):
+    for j in range(n - 1, i-1, -1):
+        print(" ", end="")
+        #print(list(range(n - 1, i-1, -1)))
+    for k in range(1, i + 1):
+        print("*", end="")
+    print()
+
